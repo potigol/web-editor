e0fc2edbed09642175e650f50e03d507e835ba9d
Pedro de Lucas Soares Cardoso <pedro.soares@academico.ifrn.edu.br>
2019-03-19 10:21:19 -0300

    :cake: create editor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import { render } from 'react-dom';
+import brace from 'brace';
+import AceEditor from 'react-ace';
 
-import App from './components/App';
+// assets
+import './assets/stylesheets/base.scss';
+
+// editor themes
+import 'brace/mode/javascript';
+import './mode/potigol';
+import 'brace/theme/tomorrow_night';
+
+// Render editor
 render(
-		<App />,
-	document.getElementById('root')
+  <AceEditor
+    mode="potigol"
+    theme="tomorrow_night"
+    name="editor-potigol"
+    editorProps={{$blockScrolling: true}}
+    width="100vw"
+    height="100vh"
+    fontSize="19px"
+    showPrintMargin={false}
+  />,
+ document.getElementById('root')
 );
