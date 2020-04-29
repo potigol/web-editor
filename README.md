81179c1c027ca4a452075e5fbeb6f82170ade68c
cripboy <cripboygit@gmail.com>
2020-04-28 22:40:32 -0300

    preset to deplay

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Potigol Web Editor
 Potigol Web Editor 🍤 é uma plataforma para edição e execução de códigos [potigol](http://potigol.github.io/) no navegador utilizando a migração do grammar original para javascript com antlr4.js. A função desse projeto é trazer uma base para o desenvolvimento de softwares voltados ao uso didático
+
+## Requisitos
+- Java VM
+- Node js
--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
-const env = process.env.NODE_ENV || "development";
-const bundlers = {
-    production: "./prod",
-    development: "./dev"
+const env = process.env.NODE_ENV;
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// body parser (placeholder)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// react preset
+if(env)
+{
+    app.use(express.static(path.join(process.cwd(), "build")));
+    app.get('/*', function (req, res) {
+        res.sendFile(path.join(process.cwd(), 'build', 'index.html'));
+    });
 }
 
-require(bundlers[env]);
+// start server
+app.listen(port, () => console.log("Running", port));
